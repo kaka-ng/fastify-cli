91a2753b036b535dd202c5de72bee5f48d9972b7
KaKa <kaka@kakawebsitedemo.com>
2022-05-23 17:19:50 +0800

    fix: command

--- src/commands/generate/index.ts
+++
@@ -1,8 +0,0 @@
-import { Command } from '@oclif/core'
-
-export default class Generate extends Command {
-  static description = 'Generate fastify project'
-
-  async run (): Promise<void> {
-  }
-}
