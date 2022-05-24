15b297b20553c847eba729e98b6966839182b9a1
KaKa <kaka@kakawebsitedemo.com>
2022-05-24 14:04:06 +0800

    test: better test utils

--- test/commands/generate/project.test.ts
+++ test/commands/generate/project.test.ts
@@ -1,19 +1,33 @@
+import { randomUUID } from 'crypto'
+import { rm } from 'fs/promises'
+import { resolve } from 'path'
 import t from 'tap'
-import { runCommand } from '../../run-command'
-import { until } from '../../until'
+import { ENTER, runCommand } from '../../run-command'
 
 t.test('JavaScript + Standard', async (t) => {
+  const name = randomUUID()
+  t.plan(6)
   const { stdout, stdin } = runCommand(['generate', 'project'])
-  await until(stdout, /What is your project name?/)
-  stdin.write('foo\n')
-  await until(stdout, /Where do you want to place your project?/)
-  stdin.write('dump/foo\n')
-  await until(stdout, /Which language will you use?/)
-  stdin.write('\n')
-  await until(stdout, /Which linter would you like to use?/)
-  stdin.write('\n')
-  await until(stdout, /Which test framework would you like to use?/)
-  stdin.write('\n')
-  await until(stdout, /Do you want to run "npm install"?/)
-  stdin.write('N\n')
+  await stdout.until(/What is your project name?/)
+  stdin.writeLn(name)
+  t.pass('project name')
+  await stdout.until(/Where do you want to place your project?/)
+  stdin.writeLn(name)
+  t.pass('project location')
+  await stdout.until(/Which language will you use?/)
+  stdin.press(ENTER)
+  t.pass('project language')
+  await stdout.until(/Which linter would you like to use?/)
+  stdin.press(ENTER)
+  t.pass('project lint')
+  await stdout.until(/Which test framework would you like to use?/)
+  stdin.press(ENTER)
+  t.pass('project test framework')
+  await stdout.until(/Do you want to run "npm install"?/)
+  stdin.writeLn('N')
+  t.pass('project npm install')
+
+  t.teardown(async function () {
+    await rm(resolve(name), { recursive: true, force: true })
+  })
 })
--- test/run-command.ts
+++ test/run-command.ts
@@ -1,5 +1,43 @@
 import { ChildProcessWithoutNullStreams, spawn } from 'child_process'
 import { Writable } from 'stream'
+import { sleep } from './sleep'
+
+export const ENTER = '\n'
+export const ESC = '\u001B['
+export const KEY_UP = `${ESC}1A`
+export const KEY_DOWN = `${ESC}1B`
+
+export async function press (stream: Writable, keycode: string): Promise<void> {
+  stream.write(keycode)
+  await sleep(100)
+}
+
+export async function until (stream: CustomWritable, line: string | RegExp | ((line: string) => boolean)): Promise<void> {
+  let found = false
+  const match = compileMatch(line)
+  while (!found) {
+    await sleep(100)
+
+    const index = stream.lines.findIndex(match)
+    if (index !== -1) found = true
+  }
+  // we empty the buffer after found
+  stream.lines = []
+}
+
+export function compileMatch (line: string | RegExp | ((line: string) => boolean)): ((line: string) => boolean) {
+  if (typeof line === 'string') {
+    return function match (from: string) {
+      return from === line
+    }
+  }
+  if (line instanceof RegExp) {
+    return function match (from: string) {
+      return line.test(from)
+    }
+  }
+  return line
+}
 
 export class CustomWritable extends Writable {
   lines: string[] = []
@@ -8,17 +46,27 @@ export class CustomWritable extends Writable {
     this.lines.push(chunk.toString())
     done()
   }
+
+  async until (line: string | RegExp | ((line: string) => boolean)): Promise<void> {
+    return await until(this, line)
+  }
+}
+
+interface CustomStdIn extends Writable {
+  press: (keycode: string) => Promise<void>
+  writeLn: (line: string) => Promise<void>
 }
 
 export function runCommand (args: string[] = []): {
   stdout: CustomWritable
   stderr: CustomWritable
-  stdin: Writable
+  stdin: CustomStdIn
   child: ChildProcessWithoutNullStreams
 } {
   const child = spawn('node', ['bin/dev', ...args])
   const stderr = new CustomWritable()
   const stdout = new CustomWritable()
+  const stdin = child.stdin as CustomStdIn
   child.stdout.pipe(stdout)
   child.stderr.pipe(stderr)
 
@@ -27,5 +75,20 @@ export function runCommand (args: string[] = []): {
     stderr.end()
   })
 
-  return { stdout, stdin: child.stdin, stderr, child }
+  stdin.press = async function (keycode: string): Promise<void> {
+    stdin.write(keycode)
+    await sleep(100)
+  }
+
+  stdin.writeLn = async function (line: string): Promise<void> {
+    stdin.write(`${line}${ENTER}`)
+    await sleep
+  }
+
+  return {
+    stdout,
+    stdin,
+    stderr,
+    child
+  }
 }
