771741147a69994dbd8d6a8d8729c603e1777c6f
KaKa <kaka@kakawebsitedemo.com>
2022-06-06 18:02:59 +0800

    feat: generate plugin

--- src/utils/package-json/project.ts
+++ src/utils/package-json/project.ts
@@ -1,18 +1,4 @@
-import { resolve } from 'path'
-
-export function findPackageJSON (): {
-  version: string
-  dependencies: { [key: string]: string }
-  devDependencies: { [key: string]: string }
-} {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const pkg = require(resolve('package.json'))
-  return {
-    version: pkg.version,
-    dependencies: pkg.dependencies,
-    devDependencies: pkg.devDependencies
-  }
-}
+import { findPackageJSON, sort } from './base'
 
 export function computePackageJSON (answer: any): string {
   const pkg = findPackageJSON()
@@ -27,15 +13,6 @@ export function computePackageJSON (answer: any): string {
   }
 
   return JSON.stringify(template, null, 2)
-}
-
-function sort (dependencies: { [key: string]: string }): { [key: string]: string } {
-  const keys = Object.keys(dependencies).sort()
-  const obj: { [key: string]: string } = {}
-  for (const key of keys) {
-    obj[key] = dependencies[key]
-  }
-  return obj
 }
 
 export function computeScripts (answer: any): { [key: string]: string } {
